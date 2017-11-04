8d3b95f1fdd133134d691a78cfb8a8a455b416be
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2017-11-04 17:37:48 +0530

    minor modification

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 var server = http.createServer(app);
 
-var io = require('socket.io')(server, { path: '/node/sockets/test/' }).listen(server);
+var io = require('socket.io')(server, {path: '/node/'}).listen(server);
 
 server.listen(3700);
 
@@ -12,18 +12,17 @@ app.get('/node/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/static', express.static('node_modules'));
-
 io.on('connection', function (socket) {
     console.log("Connected succesfully to the socket ...");
 
     var news = [
-        {title: 'The cure of the Sadness is to play Videogames', date: '04.10.2016'},
+        {title: 'The cure of the Sadness is to play Videogames', date: '04.10.2016'}
     ];
 
     socket.emit('news', news);
 
-    socket.on('my other event', function (data) {
+    socket.on('message', function (data) {
         console.log(data);
+        socket.emit('message', data)
     });
 });
