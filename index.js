9f83d5a834ab00ed8726aa1523fd484fe0c283b8
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2017-11-04 23:08:59 +0530

    test

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, {path: '/node/'})(http);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3700;
 
-app.get('/node/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index1.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
 
-http.listen(port, function(){
-    console.log('listening on *:' + port);
+
+io.on('connection', function (socket) {
+    socket.on('onmessage', function (msg) {
+        io.emit('onmessage', msg);
+    });
 });
