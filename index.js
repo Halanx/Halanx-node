2360527079347d0a00e0d22627640d84c6c744a9
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2017-11-12 18:57:17 +0530

    join rooms

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 var port = process.env.PORT || 3700;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+
+app.get('/room/:rname/', function (req, res) {
+    if (req.params.rname) {
+        res.sendFile(__dirname + '/room.html');
+    } else {
+        res.redirect('/');
+    }
+});
 
 http.listen(port, function () {
     console.log('listening on *:' + port);
@@ -14,8 +26,13 @@ http.listen(port, function () {
 
 
 io.on('connection', function (socket) {
-    console.log("User Connected");
-    socket.on('onmessage', function (msg) {
-        io.emit('onmessage', msg);
+
+    socket.on('join', function (msg) {
+        socket.join(msg.room);
+    });
+
+    socket.on('onMessage', function (msg) {
+        console.log("Message Received from ", socket.id , " ",  msg);
+        socket.to(msg.room).emit('onMessage',{"text": msg.text});
     });
 });
